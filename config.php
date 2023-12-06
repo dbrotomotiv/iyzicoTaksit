03ee157a84ee37fa06703d68d2efeb3d5ff1f079
dbrotomotiv <153138332+dbrotomotiv@users.noreply.github.com>
2023-12-07 01:35:18 +0300

    Update config.php

--- config.php
+++ config.php
@@ -9,8 +9,8 @@ class Config
     public static function options()
     {
         $options = new \Iyzipay\Options();
-        $options->setApiKey('sandbox-LoDo2D258h1uXyjopUvRXj1ir4BiWyAI');
-        $options->setSecretKey('sandbox-VU1fLYVj3wyKWQ66DMEoBBjlreVUVflx');
+        $options->setApiKey('sandbox-96d784bc67ae8feea0f24d92de593f8989977e2b08dc2dc22ca33f3dfad04587');
+        $options->setSecretKey('sandbox-6e1e8a46b25772639ec868b48f0834b948b471ff1f49ed695abf38dfed2315d6');
         $options->setBaseUrl('https://sandbox-api.iyzipay.com');
 
         return $options;
